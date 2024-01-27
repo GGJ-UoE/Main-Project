80e060afddf78a7731d9884f4709bf6ff630cdfa
Goran Riddle <supersaiyangoran@gmail.com>
2024-01-27 14:04:39 +0000

    Ghost enemy with ping pong movement

---
+++ Assets/Characters/Enemies/GhostCharacter_Free/HorizontalMovingEnemy.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovingEnemy : MonoBehaviour
+{
+    [SerializeField] private float speed = 4;
+    [SerializeField] private bool faceRight = false;
+    [SerializeField] private bool oneDirection = false;
+    [SerializeField] private bool affectedByGravity = false;
+    [SerializeField] private Transform floorChecker;
+    private bool isGrounded;
+    
+    private float gravity = -9.81f;
+    private Animator anim;
+    private CharacterController controller;
+    void Start()
+    {
+        anim = transform.GetComponent<Animator>();
+        controller = transform.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //isGrounded = controller.isGrounded;
+        if (!oneDirection)
+        {
+            if (WillFall() || WillCollide())
+            {
+                FlipDirection();
+            }
+        }
+        controller.Move(Vector3.forward * Time.deltaTime * speed);
+        //Debug.Log("Will collide: " + WillCollide());
+
+    }
+
+    void FlipDirection()
+    {
+        speed = -speed;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 180, transform.eulerAngles.z);
+    }
+
+    private bool WillFall()
+    {
+        if (floorChecker == null)
+            return false;
+
+        Ray ray = new Ray(floorChecker.position + Vector3.up * 0.1f, Vector3.down);
+        float range = 0.2f;
+        return !Physics.Raycast(ray, range);
+    }
+
+    private bool WillCollide() {
+        Ray ray = new Ray(transform.position + Vector3.forward * 0.1f, Vector3.forward);
+        float range = 0.3f;
+        //Debug.DrawRay(transform.position + Vector3.forward * 0.1f, Vector3.forward, Color.green);
+        return Physics.Raycast(ray, range);
+    }
+}
