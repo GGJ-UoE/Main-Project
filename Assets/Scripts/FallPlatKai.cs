70f12957dd87f0d630364e1186b7fcd2024c9b12
Lycoris-SF <sangvis.ferri.lyco@outlook.com>
2024-01-28 13:10:19 +0000

    fix fallplat

--- Assets/Scripts/FallPlatKai.cs
+++ Assets/Scripts/FallPlatKai.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class FallPlatKai : MonoBehaviour
 {
-    //public GameObject root;
+    public GameObject root;
 	void OnCollisionEnter(Collision collision)
 	{
         Debug.Log(collision);
         //Debug.DrawRay(contact.point, contact.normal, Color.white);
         if (collision.collider.CompareTag("Player"))
         {
-            if (!transform.root.gameObject.GetComponent<Rigidbody>())
+            if (!root.GetComponent<Rigidbody>())
             {
-                transform.root.gameObject.AddComponent<Rigidbody>();
+                root.AddComponent<Rigidbody>();
             }
         }
     }
